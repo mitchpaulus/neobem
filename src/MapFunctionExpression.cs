75298972d5273a877efbe3b069d5071cbf69823d
Mitchell Paulus <paulusm14@gmail.com>
2021-01-28 22:13:17 -0600

    Add forgotten Map function file

---
+++ src/MapFunctionExpression.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace src
+{
+    public class MapFunctionExpression : FunctionExpression
+    {
+        public MapFunctionExpression() : base(new List<Dictionary<string, Expression>>(), new List<string>{ "func", "list" })
+        {
+
+        }
+
+        public override (string, Expression) Evaluate(List<Expression> inputs)
+        {
+            FunctionExpression function = inputs[0] as FunctionExpression;
+            ListExpression list = inputs[1] as ListExpression;
+
+            List<(string, Expression)> mappedList = list.Expressions
+                .Select(expression => function.Evaluate(new List<Expression>() {expression})).ToList();
+
+            var returnString = string.Join("", mappedList.Select(tuple => tuple.Item1));
+            ListExpression newListExpression = new ListExpression(mappedList.Select(tuple => tuple.Item2).ToList());
+
+            return (returnString, newListExpression);
+        }
+    }
+}

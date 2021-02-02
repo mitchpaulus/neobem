202d68487feaab3de5972c2ee6bb8d8eb00cae51
Mitchell Paulus <paulusm14@gmail.com>
2021-02-01 22:05:26 -0600

    Change characters for replacements in objects from { to <
    It turns out curly braces were not a good idea, since those already are
    commonly used in the auto generated comments from the IDFEditor.

--- src/ObjectVariableReplacer.cs
+++ src/ObjectVariableReplacer.cs
@@ -9,43 +9,48 @@ namespace src
 
         public string Replace(string objectText, List<Dictionary<string, Expression>> variables)
         {
+            var openingChar = '<';
+            var endingChar = '>';
             StringBuilder output = new StringBuilder();
 
             Stack<int> leftCurlyBraceIndicies = new Stack<int>();
 
             for (int i = 0; i < objectText.Length ; i++)
             {
-                if (objectText[i] == '{' && objectText[i  + 1] != '{')
+                if (objectText[i] == openingChar && objectText[i  + 1] != openingChar)
                 {
                     leftCurlyBraceIndicies.Push(i);
                     if (leftCurlyBraceIndicies.Count > 1)
                         throw new NotSupportedException("Nested expression replacement is not supported.");
                 }
-                else if (objectText[i] == '{' && objectText[i + 1] == '{')
+                else if (objectText[i] == openingChar && objectText[i + 1] == openingChar)
                 {
-                    output.Append('{');
+                    output.Append(openingChar);
                     i++;
                 }
-                else if (objectText[i] == '}' && objectText[i + 1] != '}')
+                else
                 {
-                    var startIndex = leftCurlyBraceIndicies.Pop();
-                    var expressionText = objectText.Substring(startIndex + 1, i - startIndex - 1);
+                    if (objectText[i] == endingChar && objectText[i + 1] != endingChar)
+                    {
+                        var startIndex = leftCurlyBraceIndicies.Pop();
+                        var expressionText = objectText.Substring(startIndex + 1, i - startIndex - 1);
 
-                    IdfPlusExpVisitor expVisitor = new IdfPlusExpVisitor(variables);
+                        IdfPlusExpVisitor expVisitor = new IdfPlusExpVisitor(variables);
 
-                    var parser = expressionText.ToParser();
-                    var tree = parser.expression();
-                    var evaluatedExpression = expVisitor.Visit(tree);
-                    output.Append(evaluatedExpression.AsString());
-                }
-                else if (objectText[i] == '}' && objectText[i + 1] == '}')
-                {
-                    output.Append('}');
-                    i++;
-                }
-                else if (leftCurlyBraceIndicies.Count == 0)
-                {
-                    output.Append(objectText[i]);
+                        var parser = expressionText.ToParser();
+                        var tree = parser.expression();
+                        var evaluatedExpression = expVisitor.Visit(tree);
+                        output.Append(evaluatedExpression.AsString());
+                    }
+                    else if (objectText[i] == endingChar && objectText[i + 1] == endingChar)
+                    {
+                        output.Append(endingChar);
+                        i++;
+                    }
+                    else if (leftCurlyBraceIndicies.Count == 0)
+                    {
+                        output.Append(objectText[i]);
+                    }
                 }
             }
 

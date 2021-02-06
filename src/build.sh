369090b390380f257e0fa3aa8cfefd69840f2841
Mitchell Paulus <paulusm14@gmail.com>
2021-02-06 10:30:48 -0600

    Add build script

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+dotnet publish -r win-x64   -c Release --self-contained
+dotnet publish -r linux-x64 -c Release --self-contained
+dotnet publish -r osx-x64   -c Release --self-contained
+
+rm -f zips/*
+
+zip -j zips/bemp_win-x64.zip bin/Release/netcoreapp3.1/win-x64/publish/*
+zip -j zips/bemp_linux-x64.zip bin/Release/netcoreapp3.1/linux-x64/publish/*
+zip -j zips/bemp_osx-x64.zip bin/Release/netcoreapp3.1/osx-x64/publish/*

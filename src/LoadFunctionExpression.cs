d55856c4dfda947324b3b5e610a39b1b6d0c554f
Mitchell Paulus <paulusm14@gmail.com>
2021-02-10 21:11:34 -0600

    Add working, but not complete Excel loading functionality
    A real power feature!
    
    Needs much work:
    1. Ability to specify worksheet
    2. Ability to specify various ranges
    3. Ability to specify stopping criteria

--- src/LoadFunctionExpression.cs
+++ src/LoadFunctionExpression.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using OfficeOpenXml;
 
 namespace src
 {
@@ -30,12 +32,87 @@ namespace src
                     throw new FileNotFoundException($"The file {stringExpression.Text} could not be found.");
                 }
             }
-            else
+            else if (inputs[0] is IdfPlusObjectExpression objectExpression)
             {
-                throw new NotImplementedException($"Non string input for load function not implemented yet.");
+
+                if (objectExpression.Members["type"] is StringExpression {Text: "Excel"})
+                {
+                    string filePath = ((StringExpression) objectExpression.Members["path"]).Text;
+                    var fullFilePath = Path.GetFullPath(filePath);
+                    return ("", ExcelDataLoader.Load(fullFilePath, null, null));
+                }
+                else
+                {
+                    throw new NotImplementedException($"Non string input for load function not implemented yet.");
+                }
             }
+
+            throw new ArgumentException($"load function expects String or Object - found {inputs[0].GetType()}");
         }
 
         public override string AsString() => "Load";
+    }
+
+    public class ExcelDataLoader
+    {
+        public static ListExpression Load(string filepath, string worksheet, string range)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo fileInfo = new FileInfo(filepath);
+            using ExcelPackage excelFile = new ExcelPackage(fileInfo);
+
+            ExcelWorksheet sheet = excelFile.Workbook.Worksheets.First();
+            int startRow = sheet.Dimension.Start.Row;
+            int endRow = sheet.Dimension.End.Row;
+            int startColumn = sheet.Dimension.Start.Column;
+            int endColumn = sheet.Dimension.End.Column;
+
+            List<string> headers = new List<string>();
+
+            List<Expression> objects = new List<Expression>();
+
+            for (var row = startRow; row <= endRow; row++)
+            {
+                if (row == startRow)
+                {
+                    for (var column = startColumn; column <= endColumn; column++)
+                    {
+                        var cellValue = sheet.Cells[row, column].Text;
+                        headers.Add(cellValue);
+                    }
+                }
+                else
+                {
+                    IdfPlusObjectExpression objectExpression = new IdfPlusObjectExpression();
+                    var index = 0;
+                    for (var column = startColumn; column <= endColumn; column++)
+                    {
+                        var cellValue = sheet.Cells[row, column].Text;
+                        var header = headers[index];
+                        if (double.TryParse(cellValue, out double numericValue))
+                        {
+                            objectExpression.Members[header] = new NumericExpression(numericValue);
+                        }
+                        else if (string.Equals(cellValue, "true", StringComparison.OrdinalIgnoreCase))
+                        {
+                            objectExpression.Members[header] = new BooleanExpression(true);
+                        }
+                        else if (string.Equals(cellValue, "false", StringComparison.OrdinalIgnoreCase))
+                        {
+                            objectExpression.Members[header] = new BooleanExpression(false);
+                        }
+                        else
+                        {
+                            objectExpression.Members[header] = new StringExpression(cellValue);
+                        }
+
+                        index++;
+                    }
+                    objects.Add(objectExpression);
+                }
+            }
+
+            return new ListExpression(objects);
+        }
     }
 }

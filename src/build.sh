b896783d6bca6b8a146f55091e394f1a7515caee
Mitchell Paulus <paulusm14@gmail.com>
2021-03-13 21:26:34 -0600

    Update build.sh script to also move the latest built documentation

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 dotnet publish -r win-x64   -c Release --self-contained
 dotnet publish -r linux-x64 -c Release --self-contained
 dotnet publish -r osx-x64   -c Release --self-contained
@@ -8,3 +9,5 @@ rm -f zips/*
 zip -j zips/neobem_win-x64_"$1".zip bin/Release/netcoreapp3.1/win-x64/publish/*
 zip -j zips/neobem_linux-x64_"$1".zip bin/Release/netcoreapp3.1/linux-x64/publish/*
 zip -j zips/neobem_osx-x64_"$1".zip bin/Release/netcoreapp3.1/osx-x64/publish/*
+
+cp ../doc/doc.pdf zips/neobem_"$1".pdf

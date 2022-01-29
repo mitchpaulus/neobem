5ac4a89d2d195b452b6d8735a5651f2e05a0aef7
Mitchell Paulus <paulusm14@gmail.com>
2022-01-29 11:38:04 -0600

    Add build script for releases
    This now compiles down to a single file for ease of distribution.
    I added several new runtime targets that should cover most everyone.

--- src/build.sh
+++ src/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-for runtime in win-x64 linux-x64 osx-x64; do
-    dotnet publish -r "$runtime" -c Release --self-contained --output publish/
+rm -rf publish/*
+
+for runtime in win-x64 win-x86 win-arm win-arm64 linux-x64 linux-musl-x64 linux-arm linux-arm64 osx-x64; do
+    mkdir -p publish/"$runtime"
+    rm -rf publish/"$runtime"/*
+    # See https://github.com/dotnet/sdk/issues/5575#issuecomment-271062056
+    # for -p:DebugType=None. This prevent the .pdb files from being added to the publish output
+    dotnet publish -r "$runtime" -o publish/"$runtime" -c Release -p:DebugType=None --self-contained
+
+    zip -r -j publish/"$runtime".zip -r publish/"$runtime"/*
+    rm -rf publish/"$runtime"
 done
 
-#dotnet publish -r linux-x64 -c Release --self-contained
-#dotnet publish -r osx-x64   -c Release --self-contained
+redo ../doc/all
 
-rm -f zips/*
-
-zip -j zips/neobem_win-x64_"$1".zip bin/Release/netcoreapp3.1/win-x64/publish/*
-zip -j zips/neobem_linux-x64_"$1".zip bin/Release/netcoreapp3.1/linux-x64/publish/*
-zip -j zips/neobem_osx-x64_"$1".zip bin/Release/netcoreapp3.1/osx-x64/publish/*
-
-cp ../doc/doc.pdf zips/neobem_"$1".pdf
+cp ../doc/doc.pdf publish/neobem.pdf

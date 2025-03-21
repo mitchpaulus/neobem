6b79e82129409617c76d04b3efad2372a454ab6c
Mitchell Paulus <paulusm14@gmail.com>
2025-03-21 17:12:06 -0500

    Add line number to --object output

--- src/EnergyPlusObjectListener.cs
+++ src/EnergyPlusObjectListener.cs
@@ -8,6 +8,9 @@ public class EnergyPlusObjectListener : NeobemParserBaseListener
 {
     public override void EnterObject(NeobemParser.ObjectContext context)
     {
+        var lineNum = context.OBJECT_TYPE().Symbol.Line;
+        Console.Write(lineNum);
+        Console.Write('\t');
         Console.Write(context.OBJECT_TYPE().GetText().Replace("\t", "\\t"));
         foreach (var f in context.FIELD())
         {

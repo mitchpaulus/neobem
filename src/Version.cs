44ec5e0edb821734ca8295f26cfdf750056bd469
Mitchell Paulus <paulusm14@gmail.com>
2026-07-09 14:24:40 -0500

    Add packaging action

--- src/Version.cs
+++ src/Version.cs
@@ -1,11 +1,18 @@
+using System.Reflection;
+
 namespace src
 {
     public static class Version
     {
         public static string Num()
         {
-            return "0.13.0";
+            string version = Assembly.GetEntryAssembly()
+                ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion ?? "unknown";
+
+            // The SDK appends '+<git commit hash>' to the informational version.
+            int plusIndex = version.IndexOf('+');
+            return plusIndex >= 0 ? version[..plusIndex] : version;
         }
     }
 }
-
